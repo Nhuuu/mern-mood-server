e9cf12e94a60d3b9d44e18df779b74e0d89e5658
chidrome <chidrome@gmail.com>
2019-02-06 14:48:35 -0800

    adding spotify route

--- controllers/result.js
+++ controllers/result.js
@@ -3,18 +3,26 @@ const request = require('request');
 const express = require('express');
 const router = express.Router();
 const db = require('../models');
-
-// yelp
 const yelp = require('yelp-fusion')
+const Spotify = require('node-spotify-api');
+
+
+// clients
 const client = yelp.client(process.env.yelpKey);
+const spotify = new Spotify({
+    id: process.env.SPOTIFY_API_KEY,
+    secret: process.env.SPOTIFY_CLIENT_SECRET
+  });
 
 
 router.get('/', (req, res)=> {
     res.send('This is the result STUB')
 })
 
+// TODO: PROTECT THE ROUTES SO THAT USER INFO CAN GET PASSED TO THE BACKEND
 // testing the API request call for darksky
 router.get('/weather', (req, res)=>{
+    // TODO: convert location to geocode
 	request('https://api.darksky.net/forecast/2931a90b9260455bec5edfa409bf0bc0/47.6062,-122.3321', function(error, response, body) {
         let results = JSON.parse(body)
         res.send(results)
@@ -23,23 +31,28 @@ router.get('/weather', (req, res)=>{
 })
 
 // write API call for YELP API call
-router.get('/restaurant', (req, res)=>{
-    console.log(req.user)
+router.post('/restaurant', (req, res)=>{
+    console.log(req.body)
     client.search({
         term: 'food', //not sure what we want this to be
-        location: 'Seattle, WA',
+        location: 'Seattle, WA', // protect the route
         limit: 10,
         open_now: true
     })
     .then((data)=>{
         let results = JSON.parse(data.body)
         let businesses = results.businesses //JSON.parse(data.body.businesses)
-        console.log(businesses)
         res.send(businesses)
     })
     .catch((error)=>{
         console.log('ERROR!', error);
     })
+})
+
+
+/// TODO: write spotify route
+router.post('/music', (req, res) => {
+    console.log('rou')
 })
 
 module.exports = router
