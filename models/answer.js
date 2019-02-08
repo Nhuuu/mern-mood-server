ab21eabb96812bffed6519d0846e6e587fdb4eca
idothestamping <douglas.klemp@gmail.com>
2019-02-08 10:39:30 -0800

    new answer seed and db added

--- models/answer.js
+++ models/answer.js
@@ -4,12 +4,10 @@ const mongoose = require('mongoose');
 // Question Schema
 const answerSchema = new mongoose.Schema({
   score: Number,
-  category: String,
   userId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User'
   },
-  average: Number,
   timestamp: { type: Date, default: Date.now},
 })
 
